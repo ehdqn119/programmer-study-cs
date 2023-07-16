930e42b22bca6f750fd0c1f2b8c15838679159e5
Jeong Ui Hoon <52648542+bryan0730@users.noreply.github.com>
2023-07-16 16:49:08 +0900

    Delete Algorithm/Solution directory

--- Algorithm/Solution/2021-01-12/송원석/Sort_H-Index.js
+++
@@ -1,9 +0,0 @@
-function solution(citations) {
-  citations.sort((a, b) => a - b);
-  let index = citations.findIndex((el, idx) => el >= citations.length - idx);
-
-  if (index < 0)
-    return 0;
-
-  return citations.length - index;
-}
